9eea654428b86886bffb1218fa4701f5de174c0f
JangTaeGyu <ttggbbgg2@gmail.com>
2024-06-03 20:25:42 +0900

    health api 추가

---
+++ core/core-api/src/main/java/com/example/multimodule/core/api/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.example.multimodule.core.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public ResponseEntity<Object> health() {
+        return ResponseEntity.ok("OK");
+    }
+}
