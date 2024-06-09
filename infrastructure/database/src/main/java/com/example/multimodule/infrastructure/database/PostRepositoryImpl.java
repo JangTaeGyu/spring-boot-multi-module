9204f96cf34356e3bbf2294cc45d14f94c1247f3
JangTaeGyu <ttggbbgg2@gmail.com>
2024-06-09 16:48:15 +0900

    post 생성 repository 구현

--- infrastructure/database/src/main/java/com/example/multimodule/infrastructure/database/PostRepositoryImpl.java
+++ infrastructure/database/src/main/java/com/example/multimodule/infrastructure/database/PostRepositoryImpl.java
@@ -2,6 +2,8 @@ package com.example.multimodule.infrastructure.database;
 
 import com.example.multimodule.core.domain.domain.post.Post;
 import com.example.multimodule.core.domain.domain.post.PostRepository;
+import com.example.multimodule.infrastructure.database.entity.PostEntity;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,9 +12,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class PostRepositoryImpl implements PostRepository {
+    private final JpaPostRepository postRepository;
+    private final JPAQueryFactory query;
 
+    @Transactional
     @Override
     public Long create(Post post) {
-        return 0L;
+        PostEntity entity = new PostEntity(post);
+        return postRepository.save(entity).getId();
     }
 }
