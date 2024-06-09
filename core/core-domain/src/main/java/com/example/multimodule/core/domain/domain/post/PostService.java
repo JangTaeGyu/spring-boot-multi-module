a1668e5957c583e58f6b22b84a2a7cc56847df58
JangTaeGyu <ttggbbgg2@gmail.com>
2024-06-09 20:33:22 +0900

    transaction handler 적용

--- core/core-domain/src/main/java/com/example/multimodule/core/domain/domain/post/PostService.java
+++ core/core-domain/src/main/java/com/example/multimodule/core/domain/domain/post/PostService.java
@@ -1,5 +1,6 @@
 package com.example.multimodule.core.domain.domain.post;
 
+import com.example.multimodule.core.domain.support.transaction.TransactionHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,11 +8,16 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PostService {
     private final PostCreator postCreator;
+
     private final PostTagManager postTagManager;
 
+    private final TransactionHandler transactionHandler;
+
     public Long createPost(PostData data, PostTagData tagData) {
-        Long createdPostId = postCreator.createPost(data);
-        postTagManager.attachTagsToPost(createdPostId, tagData);
-        return createdPostId;
+        return transactionHandler.execute(() -> {
+            Long createdPostId = postCreator.createPost(data);
+            postTagManager.attachTagsToPost(createdPostId, tagData);
+            return createdPostId;
+        });
     }
 }
