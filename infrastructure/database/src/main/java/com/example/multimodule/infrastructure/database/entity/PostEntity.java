e2c403d4ad75df7b240a6de1902028c5c228b356
JangTaeGyu <ttggbbgg2@gmail.com>
2024-06-09 16:44:03 +0900

    post entity 추가

---
+++ infrastructure/database/src/main/java/com/example/multimodule/infrastructure/database/entity/PostEntity.java
@@ -0,0 +1,39 @@
+package com.example.multimodule.infrastructure.database.entity;
+
+import com.example.multimodule.core.domain.domain.post.Post;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity @Table(name = "posts")
+@Getter @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicUpdate
+public class PostEntity extends BaseEntity {
+    @Column(nullable = false)
+    private Long categoryId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition="TEXT")
+    private String body;
+
+    @ColumnDefault("'false'")
+    private boolean show;
+
+    protected LocalDateTime deletedAt;
+
+    public PostEntity(Post post) {
+        this.categoryId = post.getCategory().getId();
+        this.title = post.getTitle();
+        this.body = post.getBody();
+        this.show = post.isShow();
+    }
+}
