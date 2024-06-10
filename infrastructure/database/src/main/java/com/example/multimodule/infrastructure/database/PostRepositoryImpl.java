cb9e2446bcd27f1e2a175bf83e9bf85cdbe969aa
JangTaeGyu <ttggbbgg2@gmail.com>
2024-06-10 20:16:01 +0900

    post repository 조회 추가

--- infrastructure/database/src/main/java/com/example/multimodule/infrastructure/database/PostRepositoryImpl.java
+++ infrastructure/database/src/main/java/com/example/multimodule/infrastructure/database/PostRepositoryImpl.java
@@ -3,10 +3,17 @@ package com.example.multimodule.infrastructure.database;
 import com.example.multimodule.core.domain.domain.post.Post;
 import com.example.multimodule.core.domain.domain.post.PostRepository;
 import com.example.multimodule.infrastructure.database.entity.PostEntity;
+import com.querydsl.core.types.ConstructorExpression;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static com.example.multimodule.infrastructure.database.entity.QCategoryEntity.categoryEntity;
+import static com.example.multimodule.infrastructure.database.entity.QPostEntity.postEntity;
 
 @Repository
 @Transactional(readOnly = true)
@@ -15,6 +22,19 @@ public class PostRepositoryImpl implements PostRepository {
     private final JpaPostRepository postRepository;
     private final JPAQueryFactory query;
 
+    private ConstructorExpression<Post> selectFields() {
+        return Projections.constructor(Post.class,
+                postEntity.id,
+                categoryEntity.id,
+                categoryEntity.name,
+                postEntity.title,
+                postEntity.body,
+                postEntity.show,
+                postEntity.createdAt,
+                postEntity.updatedAt
+        );
+    }
+
     @Transactional
     @Override
     public Long create(Post post) {
@@ -22,7 +42,17 @@ public class PostRepositoryImpl implements PostRepository {
         return postRepository.save(entity).getId();
     }
 
-
-
-
+    @Override
+    public Optional<Post> findById(Long postId) {
+        return Optional.ofNullable(
+                query.select(selectFields())
+                        .from(postEntity)
+                        .innerJoin(categoryEntity).on(postEntity.categoryId.eq(categoryEntity.id))
+                        .where(
+                                postEntity.id.eq(postId),
+                                postEntity.deletedAt.isNull()
+                        )
+                        .fetchFirst()
+        );
+    }
 }
