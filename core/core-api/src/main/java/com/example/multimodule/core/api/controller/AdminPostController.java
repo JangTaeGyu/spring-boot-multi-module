c3191c6981847fccbe7e6bba9af34bc68132485a
JangTaeGyu <ttggbbgg2@gmail.com>
2024-06-10 20:16:59 +0900

    post 조회 api 추가

--- core/core-api/src/main/java/com/example/multimodule/core/api/controller/AdminPostController.java
+++ core/core-api/src/main/java/com/example/multimodule/core/api/controller/AdminPostController.java
@@ -2,14 +2,13 @@ package com.example.multimodule.core.api.controller;
 
 import com.example.multimodule.core.api.controller.request.PostInputRequest;
 import com.example.multimodule.core.api.controller.response.CreatedResponse;
+import com.example.multimodule.core.api.controller.response.SuccessfulResponse;
+import com.example.multimodule.core.domain.domain.post.Post;
 import com.example.multimodule.core.domain.domain.post.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -24,5 +23,12 @@ public class AdminPostController {
         Long postId = postService.createPost(request.toData(), request.toTagData());
         CreatedResponse response = new CreatedResponse(postId);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<?> show(@PathVariable Long postId) {
+        Post post = postService.getPost(postId);
+        SuccessfulResponse<Post> response = new SuccessfulResponse<>(post);
+        return ResponseEntity.ok(response);
     }
 }
