b06d65e0d7fa04fa0b7c5ed4834fff45a8ba7dc3
JangTaeGyu <ttggbbgg2@gmail.com>
2024-06-16 20:27:58 +0900

    post 검색 repository 추가

--- infrastructure/database/src/main/java/com/example/multimodule/infrastructure/database/PostRepositoryImpl.java
+++ infrastructure/database/src/main/java/com/example/multimodule/infrastructure/database/PostRepositoryImpl.java
@@ -2,15 +2,23 @@ package com.example.multimodule.infrastructure.database;
 
 import com.example.multimodule.core.domain.domain.post.Post;
 import com.example.multimodule.core.domain.domain.post.PostRepository;
+import com.example.multimodule.core.domain.domain.post.PostSearchData;
 import com.example.multimodule.core.domain.support.error.NotFoundException;
 import com.example.multimodule.infrastructure.database.entity.PostEntity;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.ConstructorExpression;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.example.multimodule.infrastructure.database.entity.QCategoryEntity.categoryEntity;
@@ -34,6 +42,32 @@ public class PostRepositoryImpl implements PostRepository {
                 postEntity.createdAt,
                 postEntity.updatedAt
         );
+    }
+
+    private BooleanBuilder toBooleanBuilder(PostSearchData searchData) {
+        return new BooleanBuilder(postEntity.deletedAt.isNull())
+                .and(eqCategoryId(searchData.getCategoryId()))
+                .and(eqShow(searchData.getShow()))
+                .and(containsKeyword(searchData.getKeyword()));
+    }
+
+    @Override
+    public Page<Post> searchBy(PostSearchData searchData, Pageable pageable) {
+        BooleanBuilder booleanBuilder = toBooleanBuilder(searchData);
+
+        List<Post> content = query.select(selectFields())
+                .from(postEntity)
+                .innerJoin(categoryEntity).on(postEntity.categoryId.eq(categoryEntity.id))
+                .where(booleanBuilder)
+                .offset(pageable.getOffset()).limit(pageable.getPageSize())
+                .orderBy(postEntity.createdAt.desc())
+                .fetch();
+
+        JPAQuery<Long> count = query.select(postEntity.count())
+                .from(postEntity)
+                .where(booleanBuilder);
+
+        return PageableExecutionUtils.getPage(content, pageable, count::fetchOne);
     }
 
     @Transactional
@@ -79,5 +113,20 @@ public class PostRepositoryImpl implements PostRepository {
         postRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Post", "id", id))
                 .delete();
+    }
+
+    public BooleanExpression eqCategoryId(Long categoryId) {
+        if (categoryId == null) return null;
+        return postEntity.categoryId.eq(categoryId);
+    }
+
+    public BooleanExpression eqShow(Boolean show) {
+        if (show == null) return null;
+        return postEntity.show.eq(show);
+    }
+
+    public BooleanExpression containsKeyword(String keyword) {
+        if (keyword == null || keyword.isEmpty()) return null;
+        return postEntity.title.contains(keyword).or(postEntity.body.contains(keyword));
     }
 }
