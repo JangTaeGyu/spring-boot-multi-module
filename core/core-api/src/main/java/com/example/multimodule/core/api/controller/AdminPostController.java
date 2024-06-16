27bc57fc7a5c28c811d85260a863b6e94a3410d1
JangTaeGyu <ttggbbgg2@gmail.com>
2024-06-16 20:35:35 +0900

    post 검색 api 추가

--- core/core-api/src/main/java/com/example/multimodule/core/api/controller/AdminPostController.java
+++ core/core-api/src/main/java/com/example/multimodule/core/api/controller/AdminPostController.java
@@ -1,22 +1,43 @@
 package com.example.multimodule.core.api.controller;
 
+import com.example.multimodule.core.api.controller.constant.PageConstant;
 import com.example.multimodule.core.api.controller.request.PostInputRequest;
 import com.example.multimodule.core.api.controller.response.CreatedResponse;
+import com.example.multimodule.core.api.controller.response.PaginatedData;
+import com.example.multimodule.core.api.controller.response.PaginationResponse;
 import com.example.multimodule.core.api.controller.response.SuccessfulResponse;
 import com.example.multimodule.core.domain.domain.post.Post;
+import com.example.multimodule.core.domain.domain.post.PostSearchData;
 import com.example.multimodule.core.domain.domain.post.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin/posts")
 @RequiredArgsConstructor
 public class AdminPostController {
     private final PostService postService;
+
+    @GetMapping
+    public ResponseEntity<?> search(
+            @RequestParam(defaultValue = "", required = false) Long categoryId,
+            @RequestParam(defaultValue = "", required = false) Boolean show,
+            @RequestParam(defaultValue = "", required = false) String keyword,
+            @RequestParam(defaultValue = PageConstant.DEFAULT_PAGE, required = false) int page,
+            @RequestParam(defaultValue = PageConstant.DEFAULT_PER_PAGE, required = false) int perPage
+    ) {
+        PostSearchData request = new PostSearchData(categoryId, show, keyword);
+        Page<Post> pagePost = postService.searchPostsBy(request, PageRequest.of(page, perPage));
+        PaginationResponse<List<Post>> response = new PaginationResponse<>(pagePost.getContent(), PaginatedData.of(pagePost));
+        return ResponseEntity.ok(response);
+    }
 
     @PostMapping
     public ResponseEntity<CreatedResponse> create(@RequestBody @Valid PostInputRequest request) {
---
+++ core/core-api/src/main/java/com/example/multimodule/core/api/controller/constant/PageConstant.java
@@ -0,0 +1,6 @@
+package com.example.multimodule.core.api.controller.constant;
+
+public class PageConstant {
+    public static final String DEFAULT_PAGE = "0";
+    public static final String DEFAULT_PER_PAGE = "20";
+}
